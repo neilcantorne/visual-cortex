61ab1c4e50314816b26164450ed289ed8a77c479
neilcantorne <cantorne.neil@gmail.com>
2022-06-29 06:54:21 +0800

    add 'Tensor' fields

--- src/neu/tensor.rs
+++ src/neu/tensor.rs
@@ -1,3 +1,9 @@
-pub struct Tensor<const D: u8> {
+use std::ptr::NonNull;
 
+use crate::neu::TensorValue;
+
+pub struct Tensor<const D: usize, T>
+    where T: TensorValue {
+    sizes: [u16; D],
+    buffer: NonNull<T>
 }
