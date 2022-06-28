1b97dc1c9d9a0c3f3bc928aeb8b38a3a31c60c70
neilcantorne <cantorne.neil@gmail.com>
2022-06-29 06:30:57 +0800

    add 'TensorValue' trait

--- src/neu/mod.rs
+++ src/neu/mod.rs
@@ -1,4 +1,5 @@
 mod tensor;
-
+mod tensor_value;
 
 pub use tensor::Tensor;
+pub use tensor_value::TensorValue;
---
+++ src/neu/tensor_value.rs
@@ -0,0 +1,19 @@
+pub trait TensorValue {
+    
+}
+
+impl TensorValue for f32 {
+
+}
+
+impl TensorValue for (f32, f32) {
+
+}
+
+impl TensorValue for (f32, f32, f32) {
+
+}
+
+impl TensorValue for (f32, f32, f32, f32) {
+
+}
