c9c8570134ee518a159b8bdb2227efa040549005
neilcantorne <cantorne.neil@gmail.com>
2022-06-29 10:22:22 +0800

    add 'LENGTH' and 'ZERO' const

--- src/neu/tensor_value.rs
+++ src/neu/tensor_value.rs
@@ -1,19 +1,24 @@
 pub trait TensorValue {
-    
+    const LENGTH: usize;
+    const ZERO: Self;
 }
 
 impl TensorValue for f32 {
-
+    const LENGTH: usize = 1;
+    const ZERO: Self = 0f32;
 }
 
 impl TensorValue for (f32, f32) {
-
+    const LENGTH: usize = 2;
+    const ZERO: Self = (0f32, 0f32);
 }
 
 impl TensorValue for (f32, f32, f32) {
-
+    const LENGTH: usize = 3;
+    const ZERO: Self = (0f32, 0f32, 0f32);
 }
 
 impl TensorValue for (f32, f32, f32, f32) {
-
+    const LENGTH: usize = 4;
+    const ZERO: Self = (0f32, 0f32, 0f32, 0f32);
 }
