54faf57b358dd90523b68d2640407cb919979d77
neilcantorne <cantorne.neil@gmail.com>
2022-06-29 10:20:07 +0800

    implement 'new' for 'Tensor'

--- src/neu/tensor.rs
+++ src/neu/tensor.rs
@@ -1,4 +1,5 @@
 use std::ptr::NonNull;
+use std::alloc::{alloc, Layout};
 
 use crate::neu::TensorValue;
 
@@ -7,3 +8,75 @@ pub struct Tensor<const D: usize, T>
     sizes: [u16; D],
     buffer: NonNull<T>
 }
+
+impl<T> Tensor<1, T>
+    where T: TensorValue {
+
+    pub unsafe fn new(s1: u16) -> Result<Self, TensorError> {
+        let ptr = {
+            let layout = Layout::array::<T>(s1 as usize).unwrap();
+            NonNull::new(alloc(layout) as *mut T)
+        };
+
+        if let Some(buffer) = ptr {
+            return Ok(Self {
+                sizes: [s1],
+                buffer
+            });
+        }
+
+        Err(TensorError{ kind: TensorErrorKind::MemoryAllocationFailed })
+    }
+}
+
+impl<T> Tensor<2, T>
+    where T: TensorValue {
+    
+    pub unsafe fn new(s1: u16, s2: u16) -> Result<Self, TensorError> {
+        let ptr = {
+            let size = 
+            s1 as usize
+            * s2 as usize;
+
+            let layout = Layout::array::<T>(size).unwrap();
+
+            NonNull::new(alloc(layout) as *mut T)
+        };
+
+        if let Some(buffer) = ptr {
+            return Ok(Self {
+                sizes: [s1, s2],
+                buffer
+            });
+        }
+
+        Err(TensorError{ kind: TensorErrorKind::MemoryAllocationFailed })
+    }
+}
+
+impl<T> Tensor<3, T>
+    where T: TensorValue {
+
+    pub unsafe fn new(s1: u16, s2: u16, s3: u16) -> Result<Self, TensorError> {
+        let ptr = {
+            let size = 
+                s1 as usize
+                * s2 as usize
+                * s3 as usize;
+
+            let layout = Layout::array::<T>(size).unwrap();
+
+            NonNull::new(alloc(layout) as *mut T)
+        };
+
+        if let Some(buffer) = ptr {
+            return Ok(Self {
+                sizes: [s1, s2, s3],
+                buffer
+            });
+        }
+
+        Err(TensorError{ kind: TensorErrorKind::MemoryAllocationFailed })
+    }
+}
+
