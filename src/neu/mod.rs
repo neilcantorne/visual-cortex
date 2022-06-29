d7796573b4fff5d4e06d3921e0c243c3da43fb71
neilcantorne <cantorne.neil@gmail.com>
2022-06-29 13:23:50 +0800

    add 'Tensor' aliases

--- src/neu/mod.rs
+++ src/neu/mod.rs
@@ -2,4 +2,8 @@ mod tensor;
 mod tensor_value;
 
 pub use tensor::Tensor;
+pub type Tensor1d<T> = Tensor<1, T>;
+pub type Tensor2d<T> = Tensor<2, T>;
+pub type Tensor3d<T> = Tensor<3, T>;
+
 pub use tensor_value::TensorValue;
