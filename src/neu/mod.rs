a8bb199f63092e13fb51212aaf3d998489f5b4cc
neilcantorne <cantorne.neil@gmail.com>
2022-06-29 16:42:41 +0800

    add vec aliases

--- src/neu/mod.rs
+++ src/neu/mod.rs
@@ -7,4 +7,8 @@ pub type Tensor2d<T> = Tensor<2, T>;
 pub type Tensor3d<T> = Tensor<3, T>;
 pub type Tensor4d<T> = Tensor<4, T>;
 
+pub type Vec2f = (f32, f32);
+pub type Vec3f = (f32, f32, f32);
+pub type Vec4f = (f32, f32, f32, f32);
+
 pub use tensor_value::TensorValue;
