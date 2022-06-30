dd46fcf2ea6907cb234a0d9f6d324699bdf909a1
neilcantorne <cantorne.neil@gmail.com>
2022-06-30 16:03:55 +0800

    implement 'StackBuffer' methods

--- src/utils/stack_buffer.rs
+++ src/utils/stack_buffer.rs
@@ -1,3 +1,5 @@
+use std::iter::Map;
+
 
 #[derive(Debug, Copy, Clone)]
 pub(crate) struct StackBuffer<const SIZE: usize, T> 
@@ -10,6 +12,16 @@ impl<const SIZE: usize, T> StackBuffer<SIZE, T>
     
     pub fn new(init_value: T) -> Self {
         Self { buffer: [init_value; SIZE] }
+    }
+
+    pub fn get_count(&self) -> usize { self.buffer.len() }
+
+    pub fn get_ptr(&mut self) -> *mut T {
+        self.buffer.as_mut_ptr()
+    }
+
+    pub fn map<U>(&self, length: usize, f: fn(T) -> U) -> Map<std::iter::Take<std::array::IntoIter<T, SIZE>>, fn(T) -> U>  {
+        self.buffer.into_iter().take(length).map(f)
     }
 }
 
