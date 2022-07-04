46e51164b5e11d1bf210bb18d2c861da883d0808
neilcantorne <cantorne.neil@gmail.com>
2022-07-04 20:37:16 +0800

    implement constructor for accelerator

--- src/neu/accelerator/mod.rs
+++ src/neu/accelerator/mod.rs
@@ -7,6 +7,14 @@ pub struct Accelerator<'a> {
     context: Rc<dyn Context + 'a>
 }
 
+impl<'a> Accelerator<'a> {
+    pub fn new(device: &super::Device) -> Self {
+        Self {
+            context: device.internal.create_context()
+        }
+    }
+}
+
 pub(super) trait Context {
     
 }
