eb10477bcfc7af61a0698082774c2ea0fecd2b5d
neilcantorne <cantorne.neil@gmail.com>
2022-07-04 16:21:13 +0800

    implement device getter functions

--- src/neu/device/device.rs
+++ src/neu/device/device.rs
@@ -38,6 +38,21 @@ impl<'a> Device<'a> {
         return devices;
     }
 
+    #[inline(always)]
+    pub fn get_name(&self) -> String { self.internal.get_name() }
+
+    #[inline(always)]
+    pub fn get_vendor(&self) -> String { self.internal.get_vendor() }
+
+    #[inline(always)]
+    pub fn get_type(&self) -> super::DeviceType { self.internal.get_type() }
+
+    #[inline(always)]
+    pub fn get_global_memory(&self) -> usize { self.internal.get_global_memory() }
+
+    #[inline(always)]
+    pub fn get_clock_rate(&self) -> f32 { self.internal.get_clock_rate() }
+
 }
 
 pub(super) trait DeviceInternal {
