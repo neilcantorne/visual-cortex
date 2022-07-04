332fe46996b178495f1e4abeb70ee0d28ba446d3
neilcantorne <cantorne.neil@gmail.com>
2022-07-04 15:55:32 +0800

    implement 'from_opencl_flag' function

--- src/neu/device/device_type.rs
+++ src/neu/device/device_type.rs
@@ -8,5 +8,12 @@ pub enum DeviceType {
 }
 
 impl DeviceType {
-    
+    pub (super) fn from_opencl_flag(device_type: cl::cl_bitfield) -> Self {
+        match device_type {
+            cl::CL_DEVICE_TYPE_GPU => Self::Gpu,
+            cl::CL_DEVICE_TYPE_CPU => Self::Cpu,
+            cl::CL_DEVICE_TYPE_ACCELERATOR => Self::Accelerator,
+            _ => Self::Unknown
+        }
+    }
 }
