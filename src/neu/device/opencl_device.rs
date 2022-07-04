6a29db4e1cb9774cf3df32cb15b63a5a5c047991
neilcantorne <cantorne.neil@gmail.com>
2022-07-04 12:43:34 +0800

    implement 'get_name' function

--- src/neu/device/opencl_device.rs
+++ src/neu/device/opencl_device.rs
@@ -7,6 +7,19 @@ pub(crate) struct OpenCLDevice {
 }
 
 impl super::device::DeviceInternal for OpenCLDevice {
+    fn get_name(&self) -> String {
+        let mut buffer = StackBuffer::<256, u8>::new(0u8);
+        let mut length = 0usize;
 
-    
+        unsafe {
+            cl::clGetDeviceInfo(
+                self.id, 
+                cl::CL_DEVICE_NAME, 
+                buffer.get_count(), 
+                buffer.get_ptr() as *mut std::ffi::c_void, 
+                &mut length);
+        }
+
+        buffer.get_string(length)
+    }
 }
