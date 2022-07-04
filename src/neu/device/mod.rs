ae03c980594b0336f1e548f724320a0a62d2febf
neilcantorne <cantorne.neil@gmail.com>
2022-07-04 20:49:46 +0800

    add 'ComputingApi' enum

--- src/neu/device/mod.rs
+++ src/neu/device/mod.rs
@@ -1,8 +1,10 @@
 mod device;
 mod device_type;
 mod opencl_device;
+mod computing_api;
 
 pub use device::Device;
 pub use device_type::DeviceType;
+pub use computing_api::ComputingApi;
 
 use opencl_device::OpenCLDevice;
