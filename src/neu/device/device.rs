c85d77b63e05d0934514433517ca5e7905ad031a
neilcantorne <cantorne.neil@gmail.com>
2022-07-04 15:36:53 +0800

    add getters

--- src/neu/device/device.rs
+++ src/neu/device/device.rs
@@ -11,4 +11,8 @@ impl<'a> Device<'a> {
 
 pub(super) trait DeviceInternal {
     fn get_name(&self) -> String;
+    fn get_vendor(&self) -> String;
+    fn get_type(&self) -> super::DeviceType;
+    fn get_global_memory(&self) -> usize;
+    fn get_clock_rate(&self) -> f32;
 }
