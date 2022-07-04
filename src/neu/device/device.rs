409baaf7cb7a28f523afbe5a8d244d80b109be5e
neilcantorne <cantorne.neil@gmail.com>
2022-07-04 20:35:04 +0800

    expose internal and add 'create_context' function

--- src/neu/device/device.rs
+++ src/neu/device/device.rs
@@ -1,10 +1,9 @@
 use std::rc::Rc;
-
 use crate::utils::StackBuffer;
 
 #[derive(Clone)]
 pub struct Device<'a> {
-    internal: Rc<dyn DeviceInternal + 'a>
+    pub(in crate::neu) internal: Rc<dyn DeviceInternal + 'a>
 }
 
 impl<'a> Device<'a> {
@@ -55,10 +54,11 @@ impl<'a> Device<'a> {
 
 }
 
-pub(super) trait DeviceInternal {
+pub(in crate::neu) trait DeviceInternal {
     fn get_name(&self) -> String;
     fn get_vendor(&self) -> String;
     fn get_type(&self) -> super::DeviceType;
     fn get_global_memory(&self) -> usize;
     fn get_clock_rate(&self) -> f32;
+    fn create_context<'a>(&self) -> Rc<dyn crate::neu::accelerator::Context + 'a>;
 }
