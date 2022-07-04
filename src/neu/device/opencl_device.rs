c3f98e31846f063d416ea3edeaa98cb437e5e5f3
neilcantorne <cantorne.neil@gmail.com>
2022-07-04 15:37:44 +0800

    implement 'get_vendor' function for OpenCL

--- src/neu/device/opencl_device.rs
+++ src/neu/device/opencl_device.rs
@@ -22,4 +22,20 @@ impl super::device::DeviceInternal for OpenCLDevice {
 
         buffer.get_string(length)
     }
+
+    fn get_vendor(&self) -> String {
+        let mut buffer = StackBuffer::<256, u8>::new(0u8);
+        let mut length = 0usize;
+
+        unsafe {
+            cl::clGetDeviceInfo(
+                self.id, 
+                cl::CL_DEVICE_VENDOR, 
+                buffer.get_count(), 
+                buffer.get_ptr() as *mut std::ffi::c_void, 
+                &mut length);
+        }
+
+        buffer.get_string(length)
+    }
 }
